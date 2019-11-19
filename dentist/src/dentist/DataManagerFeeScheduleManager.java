8a525d5c82fd99a99c6ecf4f5be9762981cbafd5
nathima1 <47651907+nathima1@users.noreply.github.com>
2019-11-18 22:51:08 -0500

    Add files via upload

---
+++ dentist/src/dentist/DataManagerFeeScheduleManager.java
@@ -0,0 +1,7 @@
+package dentist;
+
+import java.util.ArrayList;
+
+public class DataManagerFeeScheduleManager {
+	ArrayList<String> feeschedule = new ArrayList<String>();
+}
---
+++ dentist/src/dentist/DataManagerPatientManager.java
@@ -0,0 +1,7 @@
+package dentist;
+
+import java.util.ArrayList;
+
+public class DataManagerPatientManager extends Person {
+	ArrayList<String> PatientManager = new ArrayList<String>();
+}
